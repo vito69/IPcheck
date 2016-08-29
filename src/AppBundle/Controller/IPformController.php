0a930d43f2cb824037db6b70dfaff642da4c8c7e
vito69 <vito692@gmail.com>
2016-08-29 16:50:00 +0200

    Rejected commit: Default

---
+++ src/AppBundle/Controller/IPformController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use AppBundle\Utils\IspData;
+
+class IPformController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function showAction()
+    {
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+
+        $ispD = new IspData();
+        $isp = $ispD -> daneISP($ipaddress);
+
+        return $this->render('IPcheck/ipForm.html.twig', array(
+            'ipaddress' => $ipaddress, 'isp' => $isp
+        ));
+        //$number = rand(0, 100);
+    }
+
+}
+?>
