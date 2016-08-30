2c9ac0c444460c651cb0e5a32de7f3a6d3e4e195
vito69 <vito692@gmail.com>
2016-08-30 23:50:33 +0200

    Rejected commit: Default

--- src/AppBundle/Controller/IPformController.php
+++ src/AppBundle/Controller/IPformController.php
@@ -15,7 +15,7 @@ class IPformController extends Controller
      */
     public function showAction($ipad)
     {
-        if(preg_match($ipad, "([0-9]{1,3}\.){3}[0-9]{1,3}"))
+        if(preg_match('([0-9]{1,3}\.){3}[0-9]{1,3}', $ipad))
         {
             $ispD = new IspData();
             $isp = $ispD->daneISP($ipad);
