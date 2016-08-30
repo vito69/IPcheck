aecedab07325af6c3a9babc979f1cb5aa390e483
vito69 <vito692@gmail.com>
2016-08-30 22:55:22 +0200

    Rejected commit: Default

--- src/AppBundle/Controller/IPformController.php
+++ src/AppBundle/Controller/IPformController.php
@@ -11,7 +11,7 @@ use AppBundle\Utils\IspData;
 class IPformController extends Controller
 {
     /**
-     * @Route("/ipform/{ipad}")
+     * @Route("/{ipad}")
      */
     public function showAction($ipad)
     {
