95823a80bf5dc1491648dd09c6f22e82ddf0739c
vito69 <vito692@gmail.com>
2016-09-06 13:33:17 +0200

    Rejected commit: Default

--- src/AppBundle/Utils/Menu.php
+++ src/AppBundle/Utils/Menu.php
@@ -13,11 +13,13 @@ class Menu
 {
     function myMenu()
     {
+        $url = "<?php echo $view['router']->generate('app_ipcheck_show') ?>";
+        $url2 = "<?php echo $view['router']->generate('app_ipcheck_show', array('ipad' => ipaddress)) ?>";
         $menu = "<nav>";
         $menu .= "<input type='checkbox' id='mobile-menu-toggle' class='mobile-menu-toggle mobile-menu-toggle-button'>";
         $menu .= "<ul id='plain-menu' class='mobile-toggleable-menu mobile-left'>";
-        $menu .= "<li><a href=\"{{ path('app_ipcheck_show') }}\">Home</a></li>";
-        $menu .= "<li><a href=\"{{ path('app_ipform_show', {'ipad': ipaddress}) }}\">Check other IP</a></li>";
+        $menu .= "<li><a href=" .$url1. ">Home</a></li>";
+        $menu .= "<li><a href=" .$url2. ">Check other IP</a></li>";
         $menu .= "</ul>";
         $menu .= "<label class='mobile-left mobmenu-toggle' for='mobile-menu-toggle'>+</label>";
         $menu .= "</nav>";
