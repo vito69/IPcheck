391ce6679f4ee091b5df36002a19d6d59adfe232
vito69 <vito692@gmail.com>
2016-09-06 14:23:01 +0200

    Rejected commit: Default

--- src/AppBundle/Utils/Menu.php
+++ src/AppBundle/Utils/Menu.php
@@ -18,8 +18,8 @@ class Menu
         $menu = "<nav>";
         $menu .= "<input type='checkbox' id='mobile-menu-toggle' class='mobile-menu-toggle mobile-menu-toggle-button'>";
         $menu .= "<ul id='plain-menu' class='mobile-toggleable-menu mobile-left'>";
-        $menu .= "<li><a href=" .$url1. ">Home</a></li>";
-        $menu .= "<li><a href=" .$url2. ">Check other IP</a></li>";
+        $menu .= "<li><a href=\"" .$url1. "\">Home</a></li>";
+        $menu .= "<li><a href=\"" .$url2. "\">Check other IP</a></li>";
         $menu .= "</ul>";
         $menu .= "<label class='mobile-left mobmenu-toggle' for='mobile-menu-toggle'>+</label>";
         $menu .= "</nav>";
