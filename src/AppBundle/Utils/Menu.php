4e50d9a85b019c3618e1b3347e4bc01d0b7ef6c7
vito69 <vito692@gmail.com>
2016-09-06 13:15:22 +0200

    Rejected commit: Default

--- src/AppBundle/Utils/Menu.php
+++ src/AppBundle/Utils/Menu.php
@@ -19,7 +19,7 @@ class Menu
         $menu .= "<li><a href=\"{{ path('app_ipcheck_show') }}\">Home</a></li>";
         $menu .= "<li><a href=\"{{ path('app_ipform_show', {'ipad': ipaddress}) }}\">Check other IP</a></li>";
         $menu .= "</ul>";
-        $menu .= "<label class='mobile-left mobmenu-toggle' for='mobile-menu-toggle''>+</label>";
+        $menu .= "<label class='mobile-left mobmenu-toggle' for='mobile-menu-toggle'>+</label>";
         $menu .= "</nav>";
         return $menu;
     }
