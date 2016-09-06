91ddfe9ef9d6a4e283344be7ccdbea3b48034c49
vito69 <vito692@gmail.com>
2016-09-06 11:18:48 +0200

    Rejected commit: Default

--- web/js/main.js
+++ web/js/main.js
@@ -1,27 +1,4 @@
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-function checkCookie() {
-    if (readCookie('cookiesy')) {
-        document.getElementById("cookie").style.display = "none";
-    } else {
-        document.getElementById("cookie").style.display = "block";
-    }
-}
-window.onload = function() {
-    checkCookie();
-}
-function cookieFunction() {
-    document.getElementById("cookie").style.display = "none";
-    document.cookie = "cookiesy=cookiesy";
-}
+
 function jestJava() {
     var jestJ = '';
     if(deployJava.getJREs()=='') {
