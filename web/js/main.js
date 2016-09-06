d2bd9fe4c38faea2f0122150fee188037d6f45ea
vito69 <vito692@gmail.com>
2016-09-06 15:57:42 +0200

    Rejected commit: Default

--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,11 @@
-
+$(function() {
+    var pgurl = window.location.href.substr(window.location.href
+            .lastIndexOf("/")+1);
+    $("#nav ul li a").each(function(){
+        if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+            $(this).addClass("active");
+    })
+});
 function jestJava() {
     var jestJ = '';
     if(deployJava.getJREs()=='') {
