188d254ad3875bda282a1cb610c1e31921dbbfe1
vito69 <vito692@gmail.com>
2016-09-10 10:28:36 +0200

    Rejected commit: Default

--- src/AppBundle/Controller/ProxyController.php
+++ src/AppBundle/Controller/ProxyController.php
@@ -1,15 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ghost
- * Date: 06.09.16
- * Time: 17:38
- */
 
 namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
-class ProxyController
+class ProxyController extends Controller
 {
-
+    /**
+     * @Route("/proxy")
+     */
+    public function prox(){
+        ob_start();
+        include __DIR__.'/../../../web/poxy/index.php';
+        $proxyContent = ob_clean();
+        return $this->render('IPcheck/proxy.html.twig', array(
+            'proxyContent' => $proxyContent
+        ));
+    }
 }
